53bf1f9dd9fc8a9f003a710a4fd5c16929abb4f4
yukiya025 <myu.yukiyanagi@gmail.com>
2019-09-13 07:42:56 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def russian():
+
+    if request.method == 'GET':
+        return render_template('russian.html')
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        word = "делаю"
+        form_input = request.form['input']
+
+        if word == form_input:
+            return render_template('index.html',
+                               form_input="正解！")
+        else:
+            return render_template('index.html',
+                               form_input= "惜しい！正解は'делаю'です")
+if __name__ == '__main__':
+    app.run(debug=True)
