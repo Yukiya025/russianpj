bcd874ae337941644371fb6d241d61e8c9a4fa1a
yukiya025 <myu.yukiyanagi@gmail.com>
2019-10-06 11:40:12 +0900

    Abstraction

--- app.py
+++ app.py
@@ -13,7 +13,17 @@ def russian():
         return render_template('russian.html')
 
 
-@app.route('/verb', methods=['GET'])
+@app.route('/verblist', methods=['GET'])
+def verblist():
+    v_l = ["第一変化動詞", "第二変化動詞", "-ся動詞", "不規則動詞"]
+    list=v_l
+    count=len(list)
+    formname = "動詞"
+    return render_template("/verb/verblist.html",
+                            list=list,
+                            count=count,
+                            formname=formname)
+@app.route('/verblist/verb1', methods=['GET'])
 def verb():
     """
     動詞<<делать>>現在活用文法表表示
@@ -21,11 +31,11 @@ def verb():
     # question_no = random.randint(0, 5)
     session['secret_text']  = random.randint(0, 5)
     verb = ru_func.verb_p()
-    return render_template("verb_p.html",
+    return render_template("/verb/verb1_table.html",
                             verb=verb)
 
 
-@app.route('/verb/do_f', methods=['GET'])
+@app.route('/verblist/verb1/verb1_prac', methods=['GET'])
 def do_f():
     exs_l1, exs_l2, exs_l3 = ru_func.practice()
     question_no = session['secret_text']
@@ -33,7 +43,7 @@ def do_f():
     ex1 = exs_l2[question_no]
     ex2 = exs_l3[question_no]
     if request.method == 'GET':
-        return render_template('verb/do_f.html',
+        return render_template('verb/verb1_prac.html',
                                     ex0=ex0,
                                     ex1=ex1,
                                     question_no=question_no
@@ -41,7 +51,7 @@ def do_f():
     return ex0, ex1, ex2, question_no
 
 
-@app.route('/verb/do_f/<int:question_no>', methods=['POST'])
+@app.route('/verblist/verb1/verb1_prac/<int:question_no>', methods=['POST'])
 def do_fp(question_no):
     """
     ---form_inputコピペ用---
@@ -56,13 +66,13 @@ def do_fp(question_no):
     form_input = request.form['input']
     if request.method == 'POST':
         if get3 == form_input:
-            return render_template('verb/do_f.html',
+            return render_template('verb/verb1_prac.html',
                                     ex0=get1,
                                     ex1=get2,
                                     form_input= "正解！<" + get3 + ">です。",
                                     question_no=question_no)
         else:
-            return render_template('verb/do_f.html',
+            return render_template('verb/verb1_prac.html',
                                     ex0=get1,
                                     ex1=get2,
                                     form_input= "正解は <" + get3 + "> です。もう一度してみてね。",
